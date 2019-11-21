2458b1ffb6e8abcf0ad86cce33b449413a1309c8
Daniel Rozov <danielrozov@gmail.com>
2019-11-22 00:12:39 +0200

    Solved exercise week7_01.Smileys. adde the methods: printWithSmileys();, createMiddleline(); and printTheTopAndBottom

--- week7-week7_01.Smileys/src/Smileys.java
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,38 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        printWithSmileys("\\:D/");
+        printWithSmileys("87.");
+        printWithSmileys("Matti");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
     }
 
+    private static void printWithSmileys(String characterString) {
+        String middleLine = createMiddleLine(characterString);
+        int printTimes = middleLine.length() / 2 + 2;
+
+        printTheTopAndBottom(printTimes);
+
+        System.out.println(":)" + middleLine + ":)");
+
+        printTheTopAndBottom(printTimes);
+    }
+
+    private static String createMiddleLine(String characterString) {
+        String middleLine = " " + characterString + " ";
+
+        if (characterString.length() % 2 == 1) {
+            middleLine += " ";
+        }
+
+        return middleLine;
+    }
+
+    private static void printTheTopAndBottom(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(":)");
+        }
+        System.out.println();
+    }
 }
