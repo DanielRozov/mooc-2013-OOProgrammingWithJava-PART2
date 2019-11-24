7e10128dd5dbd8929b36f74b5f88fd697489b363
Daniel Rozov <danielrozov@gmail.com>
2019-11-25 00:04:37 +0200

    Added classes Change.java and Changer.java

---
+++ week7-week7_02.CharacterStringChanger/src/Changer.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+public class Changer {
+
+    ArrayList<Change> changes = new ArrayList<Change>();
+
+    public Changer() {
+        //this.changes = new ArrayList<Change>();
+    }
+
+    public void addChange(Change change) {
+        this.changes.add(change);
+    }
+
+    public String change(String characterString) {
+        for (Change change : changes) {
+            characterString = change.change(characterString);
+        }
+        return characterString;
+    }
+}
