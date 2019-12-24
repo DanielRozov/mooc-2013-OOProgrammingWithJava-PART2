91db70825ad3c2c5fdbdb493a4e3692c443171b5
Daniel Rozov <danielrozov@gmail.com>
2019-12-24 10:08:25 +0200

    Modified Main class. Added automatic test

--- week7-week7_07.Dictionary/src/Main.java
+++ week7-week7_07.Dictionary/src/Main.java
@@ -6,17 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String input = "translate\n" + "monkey\n"
+                + "translate\n" + "cheese\n"
+                + "add\n" + "cheese\n" + "juusto\n"
+                + "translate\n" + "cheese\n"
+                + "quit\n";
+
+        Scanner reader = new Scanner(input);
         Dictionary dictionary = new Dictionary();
-        dictionary.add("apina", "monkey");
-        dictionary.add("banaani", "banana");
-        dictionary.add("cembalo", "harpsichord");
-
-//        ArrayList<String> translations = dictionary.translationList();
-//        for (String translation : translations) {
-//            System.out.println(translation);
-//        }
-        Scanner reader = new Scanner(System.in);
-
+        
         TextUserInterface ui = new TextUserInterface(reader, dictionary);
         ui.start();
     }
