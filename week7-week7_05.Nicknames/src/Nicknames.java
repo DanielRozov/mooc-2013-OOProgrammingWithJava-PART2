d7946f277ee4d03bebc4169cef1c61f200dee2db
Daniel Rozov <danielrozov@gmail.com>
2019-12-24 06:56:33 +0200

    Removed unused imports in Main class

--- week7-week7_05.Nicknames/src/Nicknames.java
+++ week7-week7_05.Nicknames/src/Nicknames.java
@@ -7,7 +7,6 @@ public class Nicknames {
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
         HashMap<String, String> nicknames = new HashMap<String, String>();
-
         nicknames.put("matti", "mage");
         nicknames.put("mikael", "mixu");
         nicknames.put("arto", "arppa");
