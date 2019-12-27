0f5ff512542ad9958d0f58240c0200e5c5f00ee8
Daniel Rozov <danielrozov@gmail.com>
2019-12-27 21:55:32 +0200

    The classes CD and Book implemented.

--- week8-week8_11.BoxesAndThings/src/Book.java
+++ week8-week8_11.BoxesAndThings/src/Book.java
@@ -1,13 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Daniel
- */
-public class Book {
-    
+public class Book implements ToBeStored {
+
+    private String writer;
+    private String name;
+    private double weight;
+
+    public Book(String writer, String name, double weight) {
+        this.writer = writer;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public double weight() {
+        return this.weight;
+    }
 }
