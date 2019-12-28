ed26636b412c3b2cc21a9603f2fcd501b586efa9
Daniel Rozov <danielrozov@gmail.com>
2019-12-28 07:18:07 +0200

    Class Storehaus modified. The methods stock() and take() added

--- week8-week8_12.OnlineShop/src/Storehouse.java
+++ week8-week8_12.OnlineShop/src/Storehouse.java
@@ -5,13 +5,16 @@ import java.util.Map;
 public class Storehouse {
 
     private Map<String, Integer> products;
+    private Map<String, Integer> stocks;
 
     public Storehouse() {
         this.products = new HashMap<String, Integer>();
+        this.stocks = new HashMap<String, Integer>();
     }
 
     public void addProduct(String product, int price, int stock) {
         this.products.put(product, price);
+        this.stocks.put(product, stock);
     }
 
     public int price(String product) {
@@ -19,5 +22,25 @@ public class Storehouse {
             return -99;
         }
         return this.products.get(product);
+    }
+
+    public int stock(String product) {
+        if (!this.stocks.containsKey(product)) {
+            return 0;
+        }
+        return this.stocks.get(product);
+    }
+
+    public boolean take(String product) {
+        int stock = 0;
+        if (!this.products.containsKey(product)) {
+            return false;
+        } else if (this.stocks.get(product) > 0) {
+            stock = this.stocks.get(product) - 1;
+            this.stocks.replace(product, stock);
+            return true;
+        }
+
+        return false;
     }
 }
