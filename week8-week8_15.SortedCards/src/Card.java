820ce9d18b4bdc6bbb1704254bddec47271369bf
Daniel Rozov <danielrozov@gmail.com>
2019-12-31 08:31:10 +0200

    The method compareTo() added to the class Card.

--- week8-week8_15.SortedCards/src/Card.java
+++ week8-week8_15.SortedCards/src/Card.java
@@ -1,4 +1,5 @@
-public class Card {
+
+public class Card implements Comparable<Card> {
 
     /*
      * These are static constant variables. These variables can be used inside and outside
@@ -36,4 +37,12 @@ public class Card {
         return suit;
     }
 
+    @Override
+    public int compareTo(Card o) {
+        if (this.value - o.value == 0) {
+            return this.suit - o.suit;
+        } else {
+            return this.value - o.value;
+        }
+    }
 }
--- week8-week8_15.SortedCards/src/Main.java
+++ week8-week8_15.SortedCards/src/Main.java
@@ -3,5 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         // write some test code here
+        Card first = new Card(2, Card.DIAMONDS);
+        Card second = new Card(14, Card.CLUBS);
+        Card third = new Card(12, Card.HEARTS);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
